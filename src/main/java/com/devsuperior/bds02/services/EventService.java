cd7c91491a3fb29f8b4b6e8d0c1c451a25821966
Thiago Pereira <tppsantos@brasilseg.com.br>
2023-04-14 17:06:05 -0300

    finish TDD challenge

---
+++ src/main/java/com/devsuperior/bds02/services/EventService.java
@@ -0,0 +1,43 @@
+package com.devsuperior.bds02.services;
+
+import com.devsuperior.bds02.dto.EventDTO;
+import com.devsuperior.bds02.entities.City;
+import com.devsuperior.bds02.entities.Event;
+import com.devsuperior.bds02.repositories.CityRepository;
+import com.devsuperior.bds02.repositories.EventRepository;
+import com.devsuperior.bds02.services.exceptions.ResourceNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityNotFoundException;
+
+@Service
+public class EventService {
+    @Autowired
+    private EventRepository repository;
+
+    @Autowired
+    private CityRepository cityRepository;
+
+    @Transactional
+    public EventDTO update(Long id, EventDTO dto) {
+        try {
+            Event entity = repository.getOne(id);
+            copyToEntity(dto, entity);
+            entity = repository.save(entity);
+            return new EventDTO(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException("Id not found " + id);
+        }
+    }
+
+    private void copyToEntity(EventDTO dto, Event entity) {
+        entity.setName(dto.getName());
+        entity.setUrl(dto.getUrl());
+        entity.setDate(dto.getDate());
+
+        City city = cityRepository.getOne(dto.getCityId());
+        entity.setCity(city);
+    }
+}
